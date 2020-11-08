0cabf60275f9995f94ee58a30eaea2d193445565
Alexander <alexkolom27@gmail.com>
2020-11-08 03:29:44 +0100

    Add a second year to the footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,14 +2,16 @@ import React from 'react'
 import './Footer.css'
 
 export const Footer = () => {
+	const activeYear = new Date().getFullYear()
+
 	return (
 		<footer>
 			<div>
-				<p>Movie Planet Theater &copy; 2020</p>
+				<p>Movie Planet Theater &copy; 2020{activeYear === 2020 ? '' : `-${activeYear}`}</p>
 				<p>+1 788-111-1899</p>
 				<p>835 Market St, San Francisco, CA 94103, United States</p>
 			</div>
-			<p>Developed by Kolomiichuk &lt;o.kolomiichuk@outlook.com&gt;</p>
+			<p>o.kolomiichuk@outlook.com</p>
 		</footer>
 	)
 }
